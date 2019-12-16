150c5ce2570ae6fafaa014e6665b54c6d4704b21
haobaxucan <xucan2014>
2019-12-16 11:30:41 +0800

    ecplise测试一下

--- aeureca-server/src/main/java/com/ecpss/AeurecaServerApplication.java
+++ aeureca-server/src/main/java/com/ecpss/AeurecaServerApplication.java
@@ -7,7 +7,7 @@ import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 @EnableEurekaServer
 @SpringBootApplication
 public class AeurecaServerApplication {
-
+    //运行 localhost:8088
 	public static void main(String[] args) {
 		SpringApplication.run(AeurecaServerApplication.class, args);
 	}
